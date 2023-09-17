85f3608373a9df10d20fea75793113b034da295f
Abdul <arpro7886@gmail.com>
2023-09-17 14:55:15 +0530

    Added console.log cmd

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import { FeaturesList } from "@/components/featuresList";
 import { ShopView } from "@/components/shopView";
 
 export default function Home() {
+  console.log("Server Refreshed: Success ✅")
   return (
     <main className="select-none">
       <MainImg />
